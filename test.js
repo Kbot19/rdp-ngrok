e07f984398652e387fc29967efd93bc954bca80d
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-23 21:13:11 +0000

    Update test.js

--- test.js
+++ test.js
@@ -10,53 +10,63 @@ async function fetchData(url) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url);
 
   await page.waitForSelector('input[name=firstname]');
   await page.type('input[name=firstname]', 'Karim');
   await page.type('input[name=lastname]', 'Elyamani');
-  await page.type('input[name=reg_email__]', 'fx9egt9n@spymail.one');
-  await page.type('input[name=reg_email_confirmation__]', 'fx9egt9n@spymail.one');
+  await page.type('input[name=reg_email__]', '+212 605-685904');
+  //await page.type('input[name=reg_email_confirmation__]', 'karimfreeg@gmail.com');
   await page.type('input[name=reg_passwd__]', 'Karim2021@11');
   await page.select('select[name=birthday_day]', '1');
   await page.select('select[name=birthday_month]', '1');
   await page.select('select[name=birthday_year]', '1999');
 
-  let id = '';
-  let submitId = '';
-
   const content = await page.content();
   const cheriEx = cheerio.load(content);
-  cheriEx('input[id]').each(async (index, element) => {
+  let id = '';
+  let submitId = '';
+  cheriEx('input[id]').each((index, element) => {
     const foundId = cheriEx(element).attr('id');
     if (foundId && foundId.startsWith('u_0_5_')) {
       console.log('Found ID:', foundId);
       id = foundId;
-      await page.click(`#${foundId}`);
-      console.log('Clicked on ID:', foundId);
     }
   });
 
-  cheriEx('button[name=websubmit]').each(async (index, element) => {
+  cheriEx('button[name=websubmit]').each((index, element) => {
     const foundSubmitId = cheriEx(element).attr('id');
     if (foundSubmitId && foundSubmitId.startsWith('u_0_s_')) {
       console.log('Found Submit ID:', foundSubmitId);
       submitId = foundSubmitId;
-      await page.click(`#${foundSubmitId}`);
-      console.log('Clicked on Submit ID:', foundSubmitId);
     }
   });
 
-  console.log('The extracted ID:', id);
-  console.log('The extracted Submit ID:', submitId);
+  if (id !== '' && submitId !== '') {
+    await page.click(`#${id}`);
+    console.log('Clicked on ID:', id);
+    await page.click(`#${submitId}`);
+    console.log('Clicked on Submit ID:', submitId);
+    await page.waitForNavigation(); // انتظار التنقل إلى الصفحة الجديدة
+    console.log('Navigated to new page:', page.url());
 
-  await page.waitForSelector('div[aria-label="Continue"]');
-  const continueButton = await page.$('div[aria-label="Continue"]');
-  await continueButton.click();
+    // الآن بمجرد أن نكون في الصفحة الجديدة، يمكننا البحث عن div والنقر عليه
+    await page.waitForSelector('div');
+    await page.click('div');
+    console.log('Clicked on div.');
 
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    // الآن يمكننا الانتظار حتى يظهر زر "Continue" والنقر عليه
+    await page.waitForSelector('div[aria-label="Continue"]');
+    await page.click('div[aria-label="Continue"]');
+    console.log('Clicked on Continue.');
+
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    console.log('Screenshot taken.');
+  } else {
+    console.log('ID or Submit ID not found.');
+  }
 
   await browser.close();
 })();
