226e4805769beb7191bdc1f702035dc39603ca0f
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-23 02:21:35 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://www.facebook.com/r.php';
+
+async function fetchData(url) {
+  const result = await axios.get(url);
+  return cheerio.load(result.data);
+}
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.facebook.com/r.php');
+  await page.goto(url);
 
   await page.waitForSelector('input[name=firstname]');
   await page.type('input[name=firstname]', 'Karim');
@@ -15,21 +24,27 @@ const puppeteer = require('puppeteer');
   await page.select('select[name=birthday_month]', '1');
   await page.select('select[name=birthday_year]', '1999');
 
-  // تحديد الجنس
-  const elements = await page.evaluate(() => document.querySelectorAll('#u_0_5'))
-  const firstElementId = await page.evaluate(
-    () => document.querySelector('#u_0_5').id
-  );
-  const lastTwoDigits = firstElementId.slice(-2);
-
-  const selector = `#u_0_5_${lastTwoDigits}`;
-  await page.click(selector);
-
-  // الضغط على زر Submit
   await page.waitForSelector('button[type=submit][name=websubmit]');
   await page.click('button[type=submit][name=websubmit]');
 
-  // التقاط لقطة الشاشة
+  await page.waitForNavigation();
+
+  let id = '';
+
+  const content = await page.content();
+  const cheriEx = cheerio.load(content);
+  cheriEx('input[id]').each(async (index, element) => {
+    const foundId = cheriEx(element).attr('id');
+    if (foundId && foundId.startsWith('u_0_5_')) {
+      console.log('Found ID:', foundId);
+      id = foundId;
+      await page.click(`#${foundId}`);
+      console.log('Clicked on ID:', foundId);
+    }
+  });
+
+  console.log('The extracted ID:', id);
+
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
   await browser.close();
