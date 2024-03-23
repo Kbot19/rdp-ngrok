1b942e2f589fc14a67ae72de76429c13ecb90e21
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-23 20:22:03 +0000

    Update test.js

--- test.js
+++ test.js
@@ -49,7 +49,7 @@ async function fetchData(url) {
     console.log('Clicked on ID:', id);
     await page.click(`#${submitId}`);
     console.log('Clicked on Submit ID:', submitId);
-    await page.waitForNavigation(); // انتظار التنقل إلى الصفحة الجديدة
+    await page.waitForNavigation();
     console.log('Navigated to new page:', page.url());
     await page.screenshot({ path: 'screenshot.png', fullPage: true });
     console.log('Screenshot taken.');
