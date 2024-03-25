4fd03a7704764dc50ddc5235b645a0c512e6875a
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-25 02:42:48 +0000

    Update test.js

--- test.js
+++ test.js
@@ -110,7 +110,7 @@ async function solveCaptcha(audioSrc) {
     await new Promise(resolve => setTimeout(resolve, 1500));
 
     await page.waitForSelector('input[name="contactpoint"]');
-    await page.type('input[name="contactpoint"]', 'pshqmisnd@laste.ml');
+    await page.type('input[name="contactpoint"]', 'ieyqvhphf@emlhub.com');
 
     await page.waitForSelector('button[type="submit"]');
 
