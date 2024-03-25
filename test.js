2ce5b1f9927acacbd8b221e5e91c6edd0e73c8c8
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-25 14:47:52 +0000

    Update test.js

--- test.js
+++ test.js
@@ -69,9 +69,9 @@ async function getRandomEmail() {
   await page.goto('https://www.facebook.com/r.php');
 
   await page.waitForSelector('input[name=firstname]');
-  await page.type('input[name=firstname]', 'Ahmed');
-  await page.type('input[name=lastname]', 'Almorabiti');
-  await page.type('input[name=reg_email__]', '+1 (425) 475-7001');
+  await page.type('input[name=firstname]', 'Karim');
+  await page.type('input[name=lastname]', 'Alyamani');
+  await page.type('input[name=reg_email__]', '+12252542523');
   await page.type('input[name=reg_passwd__]', 'AhmedAhmed@11');
   await page.select('select[name=birthday_day]', '1');
   await page.select('select[name=birthday_month]', '2');
