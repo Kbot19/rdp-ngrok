cde36010afa60c43e27895768cc756922343c434
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-26 08:36:22 +0000

    Update test.js

--- test.js
+++ test.js
@@ -71,7 +71,7 @@ async function getRandomEmail() {
   await page.waitForSelector('input[name=firstname]');
   await page.type('input[name=firstname]', 'Akriam');
   await page.type('input[name=lastname]', 'Mosawi');
-  await page.type('input[name=reg_email__]', '+1 (425) 475-7007');
+  await page.type('input[name=reg_email__]', '+1 (425) 275-7182');
  // await page.type('input[name=reg_email_confirmation__]', 'karimfreeg@gmail.com');
   await page.type('input[name=reg_passwd__]', 'AhmedAhmed@11');
   await page.select('select[name=birthday_day]', '1');
@@ -186,11 +186,11 @@ if (randomEmailData) {
     
     await page.click('button[type="submit"]');
 
-    //await new Promise(resolve => setTimeout(resolve, 1500));
+    await new Promise(resolve => setTimeout(resolve, 1500));
 
-    await page.waitForNavigation();
+    //await page.waitForNavigation();
 
-    //await page.goto('https://www.facebook.com/profile.php');
+    await page.goto('https://www.facebook.com/profile.php');
 
     console.log("Email: ", randomEmail);
     console.log("Pwd : AhmedAhmed@11");
