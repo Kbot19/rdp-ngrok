14e2b99c7d9b88951c4d6a8509e779dfc685cd58
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-03-27 23:13:39 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -6,11 +6,11 @@ const puppeteer = require('puppeteer');
   
   await page.goto('https://accounts.google.com/v3/signin/identifier?hl=en-gb&ifkv=ARZ0qKJp3mev17CcDjjuQzxHizfr4-A2bWBdcjnd__Z9q8Xn-L_3BXGKXS-KWjTof5gi2ecC30MRwA&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-1543424507%3A1711009155669812&theme=mn&ddm=0');
 
-  /*await page.waitForSelector('input[name="identifier"]');
+  await page.waitForSelector('input[name="identifier"]');
 
   await page.type('input[name="identifier"]', 'karimfreegg@gmail.com');
 
-  await page.click('button[type="button"]');
+  /*await page.click('button[type="button"]');
 
   await new Promise(resolve => setTimeout(resolve, 10000));
 
@@ -21,7 +21,7 @@ const puppeteer = require('puppeteer');
 
   await new Promise(resolve => setTimeout(resolve, 10000));*/
 
-  await page.screenshot({ path: 'screenshot.png.png', fullPage: true });
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
   await browser.close();
 })();
