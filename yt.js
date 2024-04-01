68f1269822a1a6d09e645476cbd4516d4c7baeb2
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-01 00:44:35 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -2,12 +2,10 @@ const puppeteer = require('puppeteer-extra');
 const stealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(stealthPlugin());
 (async () => {
-  const browser = await puppeteer.launch({
-    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  await page.goto('https://accounts.google.com/v3/signin/identifier?hl=en-gb&ifkv=ARZ0qKJp3mev17CcDjjuQzxHizfr4-A2bWBdcjnd__Z9q8Xn-L_3BXGKXS-KWjTof5gi2ecC30MRwA&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-1543424507%3A1711009155669812&theme=mn&ddm=0');
+  await page.goto('https://translate.google.com/translate?sl=en&tl=fr&hl=en&u=https%3A%2F%2Faccounts.google.com%2Fv3%2Fsignin%2Fidentifier%3Fcontinue%3Dhttps%253A%252F%252Faccounts.google.com%252F%26followup%3Dhttps%253A%252F%252Faccounts.google.com%252F%26ifkv%3DARZ0qKK1GagRHSotRghAtRp4s9jV4jM7AM_7OCg5uW3vTlwrw50CYCcgi2HH88rjLwKuZVz-ArH0%26passive%3D1209600%26flowName%3DGlifWebSignIn%26flowEntry%3DServiceLogin%26dsh%3DS1577268669%253A1711932089041250%26theme%3Dmn%26ddm%3D0%2F&client=webapp');
 
   await page.waitForSelector('input[name="identifier"]');
 
@@ -18,7 +16,7 @@ puppeteer.use(stealthPlugin());
   await page.keyboard.press('Enter');
   //await page.click('button[type="button"]');
 
-  await page.waitForNavigation();
+  //await page.waitForNavigation();
 
   /*await new Promise(resolve => setTimeout(resolve, 300));
 
@@ -32,7 +30,7 @@ puppeteer.use(stealthPlugin());
 
   await page.keyboard.press('Enter');*/
 
-  /*await new Promise(resolve => setTimeout(resolve, 10000));
+  await new Promise(resolve => setTimeout(resolve, 10000));
 
   await page.type('input[type="password"]', 'karim20021@x1');
 
