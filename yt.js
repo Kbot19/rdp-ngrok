8dc42e0dcd9ca4b92b0ac4be91d4daca0d071807
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-01 02:14:33 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -8,8 +8,11 @@ puppeteer.use(stealthPlugin());
 
   await page.goto('https://www.tiktok.com/login/phone-or-email/email');
 
-  await page.waitForSelector('input[name="username"]');
-  await page.type('input[name="username"]', 'karimfreegg@gmail.com');
+  await page.waitForSelector('#loginContainer > div.tiktok-aa97el-DivLoginContainer.exd0a430 > form > div.tiktok-q83gm2-DivInputContainer.etcs7ny0 > input');
+
+  // Type the specified email address into the email input field
+  await page.type('#loginContainer > div.tiktok-aa97el-DivLoginContainer.exd0a430 > form > div.tiktok-q83gm2-DivInputContainer.etcs7ny0 > input', 'karimfreegg@gmail.com');
+
   await new Promise(resolve => setTimeout(resolve, 1000));
 
   await page.type('input[type="password"]', 'karim2021@11');
