727ff16c757a6840adac3aafaa07c881097dcd3d
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-01 01:43:13 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -8,12 +8,12 @@ puppeteer.use(stealthPlugin());
 
   await page.goto('https://www.tiktok.com/login/phone-or-email/email');
 
-  await page.waitForSelector('input[name="username"]');
+  /*await page.waitForSelector('input[name="username"]');
   await page.type('input[name="username"]', 'karimfreegg@gmail.com');
   await new Promise(resolve => setTimeout(resolve, 1000));
 
   await page.type('input[type="password"]', 'karim2021@11');
-  /*await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise(resolve => setTimeout(resolve, 1000));
   await page.evaluate(() => {
     document.querySelector('#loginContainer > div.tiktok-aa97el-DivLoginContainer.exd0a430 > form > button').click();
   });
