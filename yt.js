9c529bb231c8e796ecf27fd071f6506cc4431c9b
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-02 20:35:56 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--window-size=800,600', '--no-sandbox', '--disable-setuid-sandbox', '--disable-infobars', '--disable-web-security', '--disable-dev-shm-usage']
+  });
+
   const page = await browser.newPage();
-  
+
   // تعيين user agent ليكون كهاتف Android
   await page.setUserAgent('Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Mobile Safari/537.36');
 
