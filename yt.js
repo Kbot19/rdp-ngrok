22208bb376e781a13c7b17cc83788d060ea0fc08
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-03 16:08:50 +0000

    Update yt.js

--- yt.js
+++ yt.js
@@ -69,10 +69,6 @@ const puppeteer = require('puppeteer');
 
   await page.click('button[type="submit"][value="Next"]');
 
-  //await new Promise(resolve => setTimeout(resolve, 6000));
-
-  //await page.click('button[type="submit"][value="Next"]');
-
   //await page.click('input[type="radio"][value="2"][name="sex"]');
 
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
