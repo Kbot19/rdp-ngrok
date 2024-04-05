d3f791bd4afe4005b3a6f4aae84ee0e30c854080
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-05 21:20:26 +0000

    Update massar.js

--- massar.js
+++ massar.js
@@ -4,21 +4,32 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({ headless: true });
   const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1';
   let pageCount = 0;
+  let openPageCount = 0;
+  const refreshFrequency = 5;
 
   for (let i = 0; i < 50000; i++) {
     const page = await browser.newPage();
     await page.setUserAgent(userAgent);
     await page.setViewport({ width: 414, height: 896 }); // Viewport for iPhone 12 Pro Max
-    await page.goto('https://massar.men.gov.ma');
-    
-    setInterval(async () => {
-      await page.reload();
-    }, 5000);
-    
+
+    setTimeout(async () => {
+      await page.goto('https://massar.men.gov.ma');
+      openPageCount++;
+      console.log(`عدد الصفحات المفتوحة: ${openPageCount}`);
+
+      if (openPageCount % refreshFrequency === 0) {
+        console.log(`تحديث ${refreshFrequency} صفحات`);
+        const pages = await browser.pages();
+        for (const page of pages) {
+          await page.reload();
+        }
+      }
+    }, i * 1000); // تحديد فاصل زمني بين فتح كل صفحة جديدة
+
     pageCount++;
   }
 
   setInterval(() => {
-    console.log(`عدد الصفحات المفتوحة: ${pageCount}`);
+    console.log(`عدد الصفحات المفتوحة: ${openPageCount}`);
   }, 1000);
 })();
