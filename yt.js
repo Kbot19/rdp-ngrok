c141b4e7e508080a8c472d3214d6c20b37e35fb4
karimAlyamani <105087359+Kbot19@users.noreply.github.com>
2024-05-22 19:39:49 +0100

    Update yt.js

--- yt.js
+++ yt.js
@@ -1,280 +1,46 @@
-const puppeteer = require('puppeteer');
 const axios = require('axios');
+const cheerio = require('cheerio');
 const fs = require('fs');
+const path = require('path');
 
-async function getReceivedEmails(id) {
-  const link = `https://dropmail.me/api/graphql/web-test-wgq6m5i?query=query%20(%24id%3A%20ID!)%20%7Bsession(id%3A%24id)%20%7B%20addresses%20%7Baddress%7D%2C%20mails%7BheaderSubject%7D%7D%20%7D&variables=%7B%22id%22%3A%22${id}%22%7D`;
+// رابط الصفحة التي تحتوي على الفيديو
+const pageUrl = 'https://stream.fushaar.link/movie/22023/22023.mp4?wmsAuthSign=c2VydmVyX3RpbWU9NS8yMi8yMDI0IDM6Mzk6NTEgUE0maGFzaF92YWx1ZT1PWlZMdFFkbEpzeldzcmJqcUg4ZmVnPT0mdmFsaWRtaW51dGVzPTQ1MA==&nimblesessionid=138843680';
 
-  try {
-    const response = await axios.get(link);
-    const data = response.data;
-    const emails = data['data']['session']['mails'];
+axios.get(pageUrl)
+  .then(response => {
+    const $ = cheerio.load(response.data);
 
-    const fbCodes = [];
-    emails.forEach(email => {
-      const headerSubject = email.headerSubject;
-      const fbCode = extractFbCode(headerSubject);
-      if (fbCode) {
-        fbCodes.push(fbCode);
-      }
-    });
+    // استخراج رابط الفيديو من العنصر <source>
+    const videoUrl = $('video source').attr('src');
+    if (videoUrl) {
+      // اسم الملف الذي سيتم حفظ الفيديو به
+      const videoFileName = path.basename(videoUrl.split('?')[0]);
 
-    return fbCodes;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+      // تحميل الفيديو
+      axios({
+        method: 'get',
+        url: videoUrl,
+        responseType: 'stream'
+      }).then(response => {
+        const videoFilePath = path.resolve(__dirname, videoFileName);
+        const writer = fs.createWriteStream(videoFilePath);
 
-function extractFbCode(subject) {
-  const regex = /FB-(\d+)/;
-  const match = subject.match(regex);
-  return match ? match[1] : null;
-}
+        response.data.pipe(writer);
 
-async function getRandomEmail() {
-  const link = 'https://dropmail.me/api/graphql/web-test-wgq6m5i?query=mutation%20%7BintroduceSession%20%7Bid%2C%20expiresAt%2C%20addresses%20%7Baddress%7D%7D%7D';
+        writer.on('finish', () => {
+          console.log(`Video saved as ${videoFileName}`);
+        });
 
-  try {
-    const response = await axios.get(link);
-    const data = response.data;
-    const id = data['data']['introduceSession']['id'];
-    const email = data['data']['introduceSession']['addresses'][0]['address'];
-    
-    return { id, email };
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
-
-const devices = [
-  {
-    name: 'iPhone SE (2nd generation)',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 667 }
-  },
-  {
-    name: 'iPhone 12 Pro Max',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 428, height: 926 }
-  },
-  {
-    name: 'iPhone 12 Pro',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 390, height: 844 }
-  },
-  {
-    name: 'iPhone 12',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 390, height: 844 }
-  },
-  {
-    name: 'iPhone 12 mini',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 812 }
-  },
-  {
-    name: 'iPhone 11 Pro Max',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 896 }
-  },
-  {
-    name: 'iPhone 11 Pro',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 812 }
-  },
-  {
-    name: 'iPhone 11',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 896 }
-  },
-  {
-    name: 'iPhone XS Max',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 896 }
-  },
-  {
-    name: 'iPhone XS',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 812 }
-  },
-  {
-    name: 'iPhone XR',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 896 }
-  },
-  {
-    name: 'iPhone X',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 812 }
-  },
-  {
-    name: 'iPhone 8 Plus',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 736 }
-  },
-  {
-    name: 'iPhone 8',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 667 }
-  },
-  {
-    name: 'iPhone 7 Plus',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 736 }
-  },
-  {
-    name: 'iPhone 7',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 667 }
-  },
-  {
-    name: 'iPhone SE',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1',
-    viewport: { width: 320, height: 568 }
-  },
-  {
-    name: 'iPhone 6s Plus',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 736 }
-  },
-  {
-    name: 'iPhone 6s',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 667 }
-  },
-  {
-    name: 'iPhone 6 Plus',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 414, height: 736 }
-  },
-  {
-    name: 'iPhone 6',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 375, height: 667 }
-  },
-  {
-    name: 'iPhone 5s',
-    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    viewport: { width: 320, height: 568 }
-  }
-];
-
-    
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-infobars',
-      '--disable-notifications',
-      '--disable-web-security',
-      '--ignore-certificate-errors',
-      '--allow-insecure-localhost',
-      '--disable-device-emulation',
-      '--disable-device-discovery-notifications',
-      '--disable-extensions',
-      '--disable-dev-shm-usage',
-      '--disable-background-networking',
-      '--disable-background-timer-throttling',
-      '--disable-client-side-phishing-detection',
-      '--disable-default-apps',
-      '--disable-hang-monitor',
-      '--disable-popup-blocking',
-      '--disable-prompt-on-repost',
-      '--disable-sync',
-      '--disable-translate',
-      '--metrics-recording-only',
-      '--safebrowsing-disable-auto-update',
-      '--password-store=basic',
-      '--use-mock-keychain',
-      '--hide-scrollbars',
-      '--mute-audio',
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--disable-backgrounding-occluded-windows'
-    ]
+        writer.on('error', (err) => {
+          console.error('Error writing the video file:', err);
+        });
+      }).catch(err => {
+        console.error('Error downloading the video:', err);
+      });
+    } else {
+      console.error('No video source found on the page');
+    }
+  })
+  .catch(err => {
+    console.error('Error fetching the page:', err);
   });
-
-  const page = await browser.newPage();
-
-  //await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1');
-  
-  //await page.setViewport({ width: 320, height: 568 });
-
-  const randomIndex = Math.floor(Math.random() * devices.length);
-  const randomDevice = devices[randomIndex];
-
-  await page.setUserAgent(randomDevice.userAgent);
-  await page.setViewport(randomDevice.viewport);
-
-
-  await page.goto('https://m.facebook.com/reg/?app_id=124024574287414&logger_id=65ff3ac6-f305-415b-8d42-f669ac0ea6d5&is_two_steps_login=0&cid=103&next=https%3A%2F%2Fm.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Den_GB%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522k5n6mm1nlkuf7webzpp1pofbpqjnt601247orm3yd8s1a1p733%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D65ff3ac6-f305-415b-8d42-f669ac0ea6d5%26tp%3Dunspecified&locale2=en_GB&refsrc=deprecated&soft=hjk');
-
-  await page.waitForSelector('#firstname_input');
-  await page.type('#firstname_input', 'karim');
-
-  await page.waitForSelector('#lastname_input');
-  await page.type('#lastname_input', 'Elyamani');
-
-  // النقر على زر الإرسال
-  await page.click('button[type="submit"][value="Next"]');
-
-  // انتظر لمدة 60 ثانية
-  await new Promise(resolve => setTimeout(resolve, 600));
-
-  // تعيين القيمة "Jan" في القائمة المنسدلة لشهر الميلاد
-  await page.select('select[name="birthday_month"]', '1');
-
-  // تعيين القيمة "1" في القائمة المنسدلة ليوم الميلاد
-  await page.select('select[name="birthday_day"]', '1');
-
-  // تعيين القيمة "1999" في القائمة المنسدلة لسنة الميلاد
-  await page.select('select[name="birthday_year"]', '1999');
-
-  await new Promise(resolve => setTimeout(resolve, 6000));
-
-  await page.click('button[type="submit"][value="Next"]');
-
-  //await page.click('input[type="radio"][value="2"][name="sex"]');
-
-  await new Promise(resolve => setTimeout(resolve, 6000));
-
-  await page.click('a[data-sigil="switch_phone_to_email"]');
-
-  await new Promise(resolve => setTimeout(resolve, 600));
-
-  const randomEmailData = await getRandomEmail();
-  const randomEmail = randomEmailData.email;
-
-  await page.type('input[name="reg_email__"]', randomEmail);
-
-  await page.click('button[type="submit"][value="Next"]');
-
-  await new Promise(resolve => setTimeout(resolve, 600));
-
-  await page.click('input[type="radio"][value="2"][name="sex"]');
-
-  await page.click('button[type="submit"][value="Next"]');
-
-  await new Promise(resolve => setTimeout(resolve, 600));
-
-  await page.type('input[name=reg_passwd__]', 'Karim2021911@11');
-
-  await page.click('button[type="submit"][value="Sign Up"]');
-
-  await new Promise(resolve => setTimeout(resolve, 60000));
-
-  await page.waitForSelector('body');
-
-  const html = await page.content();
-
-  // حفظ الـ HTML في ملف نصي باسم "fb.html"
-  fs.writeFileSync('fb.html', html);
-  
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
-
-  await browser.close();
-})();
